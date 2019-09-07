979d3bc037d7754bc59e4a62caac29cb48a7aff6
anugrah94 <anugrahrezkifauzinasution@gmail.com>
2019-09-07 09:47:24 +0700

    integrate with serverless and lamda aws,  refactor some code and also updated the README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # express-signup-login-api
 This is signup and login API with Express JS
+
+Local run in:
+```
+http://localhost:[port]
+```
+
+AWS Deployment:
+```
+https://d0mco0d1w4.execute-api.us-east-1.amazonaws.com/dev/
+```
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const serverless = require('serverless-http');
 const chalk = require('chalk');
 const express = require('express');
 const cors = require('cors');
@@ -28,3 +29,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
 
 app.listen(port, console.log(chalk.blue('[API]: '), chalk.green('Connected to Port !!')));
+
+module.exports.handler = serverless(app);
